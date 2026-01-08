e8594f0b8f291dc463c16035c89e28b4b7da551a
Urumu_Anusha <2401600095mca@gmail.com>
2026-01-08 12:09:21 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,56 @@
-document.getElementById("ctaBtn").addEventListener("click", () => {
-  alert("🎉 Welcome! Your journey starts now!");
-});
+// Show register
+function showRegister() {
+  document.getElementById("registerBox").style.display = "block";
+}
+
+// Register
+function register() {
+  let u = document.getElementById("newUser").value;
+  let p = document.getElementById("newPass").value;
+
+  if (!u || !p) return alert("Fill details!");
+
+  localStorage.setItem("user", u);
+  localStorage.setItem("pass", p);
+
+  alert("Account Created! Login Now");
+}
+
+// Login
+function login() {
+  let u = document.getElementById("username").value;
+  let p = document.getElementById("password").value;
+
+  if (u === localStorage.getItem("user") && p === localStorage.getItem("pass")) {
+    localStorage.setItem("loggedIn", true);
+    window.location.href = "home.html";
+  } else alert("Incorrect Details");
+}
+
+// Check login
+function checkUser() {
+  if (!localStorage.getItem("loggedIn")) window.location.href = "index.html";
+  document.getElementById("userName").innerHTML = localStorage.getItem("user");
+}
+
+// Logout
+function logout() {
+  localStorage.removeItem("loggedIn");
+  window.location.href = "index.html";
+}
+
+// Quiz submit
+function submitQuiz() {
+  let score = 0;
+  if (document.querySelector('input[name="q1"]:checked')?.value === "correct") score++;
+  if (document.querySelector('input[name="q2"]:checked')?.value === "correct") score++;
+
+  document.getElementById("result").innerHTML = "Your Score: " + score + "/2";
+
+  localStorage.setItem("progress", score);
+}
+
+// Go quiz
+function gotoQuiz() {
+  window.location.href = "quiz.html";
+}
