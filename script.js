91febcb4539d16e314f45a320e05cf7ddd9a8a30
Urumu_Anusha <2401600095mca@gmail.com>
2026-01-08 11:04:16 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,9 +1,3 @@
-// Button click
 document.getElementById("ctaBtn").addEventListener("click", () => {
   alert("🎉 Welcome! Your journey starts now!");
 });
-
-// GSAP animation
-gsap.from(".title", { duration: 1, y: -50, opacity: 0 });
-gsap.from(".subtitle", { duration: 1, y: 50, opacity: 0, delay: 0.3 });
-gsap.from(".card", { duration: 1, opacity: 0, stagger: 0.2, x: -100, delay: 0.6 });
